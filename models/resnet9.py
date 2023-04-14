dcd93894d2dcf9f749ec9384316d955b99943b96
diaa-shalaby <cadmos149@gmail.com>
2023-04-14 05:28:52 +0200

    Initial commit

---
+++ models/resnet9.py
@@ -0,0 +1,45 @@
+from utils import ImageClassificationBase
+from torch import nn
+
+
+def conv_block(in_channels, out_channels, pool=False):
+    layers = [nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+              nn.BatchNorm2d(out_channels),
+              nn.ReLU(inplace=True)]
+    
+    if pool: 
+        layers.append(nn.MaxPool2d(2))
+        
+    return nn.Sequential(*layers)
+
+
+class ResNet9(ImageClassificationBase):
+    def __init__(self, in_channels, num_classes):
+        super().__init__()
+        # Input: <batch_size - currently 32> 
+        self.conv1 = conv_block(in_channels, 64)                     # bs x 64 x 64 x 64
+        self.conv2 = conv_block(64, 128, pool=True)                  # bs x 128 x 32 x 32 
+        self.res1 = nn.Sequential(conv_block(128, 128), 
+                                  conv_block(128, 128))
+
+        self.conv3 = conv_block(128, 256, pool=True)                 # bs x 256 x 16 x 16 
+        self.conv4 = conv_block(256, 512, pool=True)                 # bs x 512 x 8 x 8 
+        self.res2 = nn.Sequential(conv_block(512, 512), 
+                                  conv_block(512, 512))
+
+        self.classifier = nn.Sequential(nn.AdaptiveAvgPool2d(1),     # bs x 512 x 1 x 1 
+                                        nn.Flatten(),                # bs x 512  
+                                        nn.Dropout(0.2),
+                                        nn.Linear(512, num_classes)) # bs x num_classes
+
+    def forward(self, xb):
+        out = self.conv1(xb)
+        out = self.conv2(out)
+        out = self.res1(out) + out
+        out = self.conv3(out)
+        out = self.conv4(out)
+        out = self.res2(out) + out
+        out = self.classifier(out)
+        return out
+    
+    
---
+++ utils.py
@@ -0,0 +1,184 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision.utils import make_grid
+import matplotlib.pyplot as plt
+
+
+##################
+# Data Utilities #
+##################
+def show_batch(dl):
+    for images, labels in dl:
+        fig, ax = plt.subplots(figsize=(12, 6))
+        ax.set_xticks([]);
+        ax.set_yticks([])
+        ax.imshow(make_grid(images, nrow=16).permute(1, 2, 0))
+        break
+
+
+def get_default_device():
+    """Pick GPU if available, else CPU"""
+    if torch.cuda.is_available():
+        return torch.device('cuda')
+    else:
+        return torch.device('cpu')
+
+
+def to_device(data, device):
+    """Move tensor(s) to chosen device"""
+    if isinstance(data, (list, tuple)):
+        return [to_device(x, device) for x in data]
+    return data.to(device, non_blocking=True)
+
+
+class DeviceDataLoader():
+    """Wrap a dataloader to move data to a device"""
+
+    def __init__(self, dataloader, device):
+        self.dataloader = dataloader
+        self.device = device
+
+    def __iter__(self):
+        """Yield a batch of data after moving it to device"""
+        for batch in self.dataloader:
+            yield to_device(batch, self.device)
+
+    def __len__(self):
+        """Number of batches"""
+        return len(self.dataloader)
+
+
+###################
+# Model Utilities #
+###################
+class ImageClassificationBase(nn.Module):
+    def training_step(self, batch):
+        """Calculate loss for a batch of training data"""
+        images, labels = batch
+        out = self(images)  # Generate predictions
+        loss = F.cross_entropy(out, labels)  # Calculate loss
+        return loss
+
+    def validation_step(self, batch):
+        """Calculate loss and accuracy for a batch of validation data"""
+        images, labels = batch
+        out = self(images)  # Generate predictions
+        loss = F.cross_entropy(out, labels)  # Calculate loss
+        acc = accuracy(out, labels)  # Calculate accuracy
+        return {'val_loss': loss.detach(), 'val_acc': acc}
+
+    def validation_epoch_end(self, outputs):
+        batch_losses = [x['val_loss'] for x in outputs]
+        epoch_loss = torch.stack(batch_losses).mean()  # Combine losses
+        batch_accs = [x['val_acc'] for x in outputs]
+        epoch_acc = torch.stack(batch_accs).mean()  # Combine accuracies
+        return {'val_loss': epoch_loss.item(), 'val_acc': epoch_acc.item()}
+
+    def epoch_end(self, epoch, result):
+        print("Epoch [{}], last_lr: {:.5f}, train_loss: {:.4f}, val_loss: {:.4f}, val_acc: {:.4f}".format(
+            epoch, result['lrs'][-1], result['train_loss'], result['val_loss'], result['val_acc']))
+
+
+def conv_block(in_channels, out_channels, pool=False):
+    layers = [nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+              nn.BatchNorm2d(out_channels),
+              nn.ReLU(inplace=True)]
+    if pool: layers.append(nn.MaxPool2d(2))
+    return nn.Sequential(*layers)
+
+
+class ResNet9(ImageClassificationBase):
+    def __init__(self, in_channels, num_classes):
+        super().__init__()
+
+        self.conv1 = conv_block(in_channels, 64)
+        self.conv2 = conv_block(64, 128, pool=True)
+        self.res1 = nn.Sequential(conv_block(128, 128), conv_block(128, 128))
+
+        self.conv3 = conv_block(128, 256, pool=True)
+        self.conv4 = conv_block(256, 512, pool=True)
+        self.res2 = nn.Sequential(conv_block(512, 512), conv_block(512, 512))
+
+        self.classifier = nn.Sequential(nn.MaxPool2d(4),
+                                        nn.Flatten(),
+                                        nn.Dropout(0.2),
+                                        nn.Linear(512, num_classes))
+
+    def forward(self, xb):
+        out = self.conv1(xb)
+        out = self.conv2(out)
+        out = self.res1(out) + out
+        out = self.conv3(out)
+        out = self.conv4(out)
+        out = self.res2(out) + out
+        out = self.classifier(out)
+        return out
+    
+
+def accuracy(preds, labels):
+    """Calculate accuracy"""
+    _, preds = torch.max(preds, dim=1)
+    return torch.tensor(torch.sum(preds == labels).item() / len(preds))
+    
+
+def evaluate(model, val_loader):
+    """Evaluate the model's performance on the validation set"""
+    outputs = [model.validation_step(batch) for batch in val_loader]
+    return model.validation_epoch_end(outputs)
+
+
+def fit(epochs, lr, model, train_loader, val_loader, opt_func=torch.optim.SGD):
+    """Train the model using gradient descent"""
+    history = []
+    optimizer = opt_func(model.parameters(), lr)
+    scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=lr, epochs=epochs,
+                                                    steps_per_epoch=len(train_loader))
+    
+    for epoch in range(epochs):
+        # Training Phase
+        model.train()
+        train_losses = []
+        lrs = []
+        for batch in train_loader:
+            with torch.cuda.amp.autocast():
+                loss = model.training_step(batch)
+                
+            train_losses.append(loss)
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+            lrs.append(optimizer.param_groups[0]['lr'])
+            scheduler.step()
+
+        # Validation phase
+        result = evaluate(model, val_loader)
+        result['train_loss'] = torch.stack(train_losses).mean().item()
+        result['lrs'] = lrs
+        model.epoch_end(epoch, result)
+        history.append(result)
+    return history
+
+
+
+####################
+# Plotting Utility #
+####################
+def plot_accuracies(history):
+    accuracies = [x['val_acc'] for x in history]
+    plt.plot(accuracies, '-x')
+    plt.xlabel('epoch')
+    plt.ylabel('accuracy')
+    plt.title('Accuracy vs. No. of epochs')
+    
+
+
+def plot_losses(history):
+    train_losses = [x.get('train_loss') for x in history]
+    val_losses = [x['val_loss'] for x in history]
+    plt.plot(train_losses, '-bx')
+    plt.plot(val_losses, '-rx')
+    plt.xlabel('epoch')
+    plt.ylabel('loss')
+    plt.legend(['Training', 'Validation'])
+    plt.title('Loss vs. No. of epochs')
